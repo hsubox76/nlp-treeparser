32bb3f65880e47811b022761dcb2d41a2f0aaf06
Christina Holland <hsubox@gmail.com>
2015-06-02 14:26:52 -0700

    Working on one test sentence!

--- lsu.js
+++ lsu.js
@@ -1,12 +1,3 @@
-var pos = require('pos');
-
-var natural = require('natural'),
-  tokenizer = new natural.TreebankWordTokenizer();
-
-var words = tokenizer.tokenize("Economic news had little effect on financial markets.");
-
-var taggedWords = new pos.Tagger().tag(words);
-
 var List = function () {
   this.head = null;
   this.tail = null;
@@ -50,10 +41,7 @@ List.prototype.removeHead = function () {
 };
 
 List.prototype.remove = function (value) {
-  console.log('value to remove:');
-  console.log(value);
   var curNode = this.head;
-  console.log(curNode.value.index, value.index);
   while (curNode) {
     if (curNode.value.index === value.index) {
       if (curNode.prev) {
@@ -80,9 +68,40 @@ List.prototype.print = function () {
     results.push(curNode.value.word);
     curNode = curNode.next;
   }
-  console.log(results.join(','));
+  console.log('| ' + results.join(',') + ' |');
 };
 
+var Queue = function () {
+  this.storage = {};
+  this.first = 0;
+  this.last = 0;
+};
+
+Queue.prototype.enqueue = function (value) {
+  this.storage[this.last] = value;
+  this.last++;
+};
+
+Queue.prototype.dequeue = function () {
+  if (this.last - this.first <= 0) {
+    return null;
+  }
+  var tmp = this.storage[this.first];
+  delete this.storage[this.first];
+  this.first++;
+  return tmp;
+};
+
+Queue.prototype.peek = function () {
+  if (this.last - this.first <= 0) {
+    return null;
+  }
+  return this.storage[this.first];
+};
+
+Queue.prototype.length = function () {
+  return this.last - this.first;
+};
 var Tree = function (value) {
   this.value = value;
   this.children = [];
@@ -110,8 +129,8 @@ Tree.prototype.print = function () {
     if (!rows[depth]) {
       rows[depth] = "";
     }
-    var x = node.parent ? node.parent.word : 'null';
-    rows[depth] = rows[depth].concat(' [ ' + node.word + ' | ' + node.pos + ' | ' + x + ' ] ');
+    var x = node.parent ? node.parent.value.word : 'null';
+    rows[depth] = rows[depth].concat(' [ ' + node.value.word + ' | ' + node.value.tag + ' | ' + x + ' ] ');
     for (var i = 0; i < node.children.length; i++) {
       queue.enqueue([node.children[i], depth+1]);
     }
@@ -125,45 +144,18 @@ Tree.prototype.print = function () {
     console.log(row);
   });
 };
-// test list
-// var list = new List();
-// list.add(1);
-// list.add(2);
-// list.add(3);
-
-// var curNode = list.tail;
-// while(curNode) {
-//   console.log(curNode.value);
-//   curNode = curNode.prev;
-// }
-
-// second tag pass looking for negatives?
-
-taggedWords = taggedWords.map(function(word, i) {
-  if (word[0] === "'s") {
-    word[1] = 'POS';
-  }
-  if (word[0] === "ca" && taggedWords[i+1][0] === "n't") {
-    word[0] = "can";
-    word[1] = "VB";
-  }
-  if (i > 0) {
-    // like and love should be verbs if they come right after a noun
-    if (word[0] === 'like' || word[0] === 'likes' || word[0] === 'love' || word[0] === 'loves' ||
-      word[0] === 'liked' || word[0] === 'loved') {
-      var prevTag = taggedWords[i-1][1];
-      if (prevTag === 'NN' || prevTag === 'NNP' || prevTag === 'NNPS' || prevTag === 'NNS' || prevTag === 'PRP') {
-        word[1] = 'VB';
-      }
-    }
-  }
-  return {word: word[0], tag: word[1], index: i};
-});
 
 var simplifyPOS = function (pos) {
   //console.log('simplifying ' + pos);
   var simplePos;
   switch (pos) {
+    case 'CC':
+      simplePos = 'conj';
+      break;
+    case 'PP$': // possessive pronoun
+    case 'PDT': // predeterminer
+    case 'DT': // determiner
+    case 'CD': // cardinal number
     case 'JJ': // adjective
     case 'JJR': // adj, comparative
     case 'JJS': // adj, superlative
@@ -179,6 +171,7 @@ var simplifyPOS = function (pos) {
     case 'RB': //adverb
       simplePos = 'adv';
       break;
+    case 'MD': // modal (can, should)
     case 'VB': // verb base form
     case 'VBD': // verb past tense
     case 'VBP': // verb present
@@ -199,11 +192,13 @@ var simplifyPOS = function (pos) {
   return simplePos;
 };
 
-var dependsOn = function (word1, word2) {
+var dependsOn = function (word1, word2, dir) {
+  dir = dir || 'pre';
+
   var pos1 = simplifyPOS(word1.tag);
   var pos2 = simplifyPOS(word2.tag);
 
-  var relationships = {
+  var preRelationships = {
     noun: {
       adj: -1,
       verb: 1,
@@ -226,14 +221,41 @@ var dependsOn = function (word1, word2) {
       prep: 1
     }
   };
-  return relationships[pos1][pos2];
+
+  var postRelationships = {
+    noun: {
+      adj: -1,
+      verb: 1,
+      prep: 1
+    },
+    verb: {
+      noun: -1,
+      adv: -1
+    },
+    adj: {
+      noun: 1
+    },
+    prep: {
+      noun: 1
+    },
+    ".": {
+      verb: 1,
+      noun: 1,
+      adj: 1,
+      prep: 1
+    }
+  };
+
+  if (dir === 'pre') {
+    return preRelationships[pos1][pos2];
+  } else if (dir === 'post') {
+    return postRelationships[pos1][pos2];
+  }
 };
 
 var outranks = function (word1, word2) {
   var pos1 = simplifyPOS(word1.tag);
   var pos2 = simplifyPOS(word2.tag);
-
-  console.log(pos1, pos2);
 
   var outranks = {
     noun: {
@@ -271,80 +293,124 @@ var outranks = function (word1, word2) {
   return outranks[pos1][pos2];
 };
 
-
-var headList = new List();
-var wordList = new List();
-var deps = {};
-
-// fill both lists with words
-// taggedWords.forEach(function (item) {
-//   headList.addToHead(item);
-//   wordList.addToHead(item);
-// });
-
-taggedWords.forEach(function (w, index) {
-  // item = next word to be parsed
-  wNode = wordList.addToHead(w);
-
-  var d = headList.head;
-
-  while (d) {
-    console.log('---------');
-    console.log('trying to find a head for ' + d.value.word);
-    if (dependsOn(d.value, w) > 0) {
-      console.log(d.value.word + ' depends on ' + w.word);
-      headList.print();
-      wordList.print();
-      if (deps[w.index]) {
-        deps[w.index].push(d.value.index);
-      } else {
-        deps[w.index] = [d.value.index];
-      }
-      console.log('removing ' + d.value.word + ' from headlist');
-      headList.remove(d.value);
-      headList.print();
-    } else {
-      break;
+var processWords = function (words) {
+  return words.map(function(word, i) {
+    if (word[0] === "'s") {
+      word[1] = 'POS';
     }
-    d = d.next;
-  }
-
-  var h = wordList.head;
-  var hFound = false;
-  var strikes = 0;
-
-  while (h) {
-    console.log('---------');
-    console.log('trying to find a head for ' + w.word);
-    console.log('checking against ' + h.value.word);
-    if (h.value.index > w.index - 1) {
-      h = h.next;
-      continue;
+    if (word[0] === "ca" && words[i+1][0] === "n't") {
+      word[0] = "can";
+      word[1] = "VB";
     }
-    if (dependsOn(w, h.value) > 0) {
-      console.log(w.word + ' depends on ' + h.value.word);
-      headList.print();
-      wordList.print();
-      if (deps[h.value.index]) {
-        deps[h.value.index].push(w.index);
-      } else {
-        deps[h.value.index] = [w.index];
+    if (i > 0) {
+      // like and love should be verbs if they come right after a noun
+      if (word[0] === 'like' || word[0] === 'likes' || word[0] === 'love' || word[0] === 'loves' ||
+        word[0] === 'liked' || word[0] === 'loved') {
+        var prevTag = words[i-1][1];
+        if (prevTag === 'NN' || prevTag === 'NNP' || prevTag === 'NNPS' || prevTag === 'NNS' || prevTag === 'PRP') {
+          word[1] = 'VB';
+        }
       }
-      hFound = true;
-      break;
-    } else {
-      if (outranks(h.value,w) > 0) {
-        console.log(outranks(h.value, w));
-        console.log('breaking because ' + h.value.word + ' outranks ' + w.word);
+    }
+    return {word: word[0], tag: word[1], index: i};
+  });
+};
+
+
+var getArcs = function(wordData) {
+  var headList = new List();
+  var wordList = new List();
+  var deps = {};
+
+  wordData.forEach(function (w, index) {
+    // item = next word to be parsed
+    wNode = wordList.addToHead(w);
+
+    var d = headList.head;
+
+    // seeing if this word can be a parent of any preceding words
+    while (d) {
+      if (dependsOn(d.value, w, 'pre') > 0) {
+        console.log(d.value.word + ' depends on ' + w.word);
+        headList.print();
+        wordList.print();
+        if (deps[w.index]) {
+          deps[w.index].push(d.value.index);
+        } else {
+          deps[w.index] = [d.value.index];
+        }
+        console.log('removing ' + d.value.word + ' from headlist');
+        headList.remove(d.value);
+      } else {
         break;
       }
+      d = d.next;
     }
-    h = h.next;
-  }
 
-  if (!hFound) {
-    headList.addToHead(w);
-  }
-});
+    var h = wordList.head;
+    var hFound = false;
+    var strikes = 0;
 
-console.log(deps);
+    // seeing if this word is a dependent of any preceding words
+    while (h) {
+      if (w.word === 'on') {
+        console.log('trying to find a head for on');
+        console.log('trying ' + h.value.word);
+        console.log(dependsOn(w, h.value, 'post'));
+      }
+      if (h.value.index > w.index - 1) {
+        h = h.next;
+        continue;
+      }
+      if (dependsOn(w, h.value, 'post') > 0) {
+        console.log(w.word + ' depends on ' + h.value.word);
+        headList.print();
+        wordList.print();
+        if (deps[h.value.index]) {
+          deps[h.value.index].push(w.index);
+        } else {
+          deps[h.value.index] = [w.index];
+        }
+        hFound = true;
+        break;
+      } else {
+        if (outranks(h.value,w) > 0) {
+          console.log('breaking because ' + h.value.word + ' outranks ' + w.word);
+          break;
+        }
+      }
+      h = h.next;
+    }
+
+    if (!hFound) {
+      headList.addToHead(w);
+    }
+  });
+  return { head: headList.head, deps: deps };
+};
+
+// build a tree
+var parse = function(wordData) {
+  var arcData = getArcs(wordData);
+  // find root
+  var arcs = arcData.deps;
+  var root = arcData.head;
+  var tree = new Tree(root.value);
+  console.log(root);
+  var recurse = function (head, node) {
+    arcs[head].forEach( function (dependent) {
+      var newNode = node.addChild(wordData[dependent]);
+      if (arcs[dependent]) {
+        recurse(dependent, newNode);
+      }
+    });
+  };
+  recurse(root.value.index, tree);
+  tree.print();
+};
+
+exports.getArcs = getArcs;
+exports.parse = parse;
+exports.processWords = processWords;
+// parse(taggedWords);
+
